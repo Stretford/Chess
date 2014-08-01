de654f9d60ba67333a8afb63652d9f23e2f6b982
Stretford <stretford@aas-MacBook-Pro.local>
2014-08-01 19:23:42 +0800

    Initial Commit

---
+++ Chess/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Chess
+//
+//  Created by NStretford on 8/1/14.
+//  Copyright (c) 2014 NStretford. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
